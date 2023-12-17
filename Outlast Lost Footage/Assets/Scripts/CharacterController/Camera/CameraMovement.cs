9ef047bd8f06aa0af0f6eb6adf523e3b281e3571
OmarHidoussi <gover8603@gmail.com>
2023-12-17 23:29:12 +0100

    Upload 5.33

--- Outlast Lost Footage/Assets/Scripts/CharacterController/Camera/CameraMovement.cs
+++ Outlast Lost Footage/Assets/Scripts/CharacterController/Camera/CameraMovement.cs
@@ -10,6 +10,9 @@ public class CameraMovement : MonoBehaviour
     public float Sensetivity;
     public float TransitionSpeed;
     public Transform PlayerBody;
+    public Transform LookBackDirection;
+    public bool Lookback;
+    public float LookbackTransitionSpeed;
     public float X_Max, X_Min;
 
     public InputManager input;
@@ -46,20 +49,24 @@ public class CameraMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        float MouseX = Input.GetAxis("Mouse X") * Time.deltaTime * Sensetivity;
-        float MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * Sensetivity * Factor;
+        if (!Lookback)
+        {
+            float MouseX = Input.GetAxis("Mouse X") * Time.deltaTime * Sensetivity;
+            float MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * Sensetivity * Factor;
 
 
-        xRotation -= MouseY;
-        xRotation = Mathf.Clamp(xRotation, X_Min, X_Max);
+            xRotation -= MouseY;
+            xRotation = Mathf.Clamp(xRotation, X_Min, X_Max);
 
-        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
-        PlayerBody.Rotate(Vector3.up * MouseX);
+            PlayerBody.Rotate(Vector3.up * MouseX);
+        }
 
         HandleHeight();
+        HandleLookback();
     }
 
     #endregion
@@ -75,6 +82,78 @@ public class CameraMovement : MonoBehaviour
         }
         else
             transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, col.center.y, 0) + offset, 50f);
+    }
+
+    /*void HandleLookback()
+    {
+        Lookback = Input.GetKey(KeyCode.LeftAlt);
+
+        if (Lookback)
+        {
+            Quaternion lookBackRotation = Quaternion.LookRotation(LookBackDirection.forward, transform.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookBackRotation, Time.deltaTime * LookbackTransitionSpeed);
+        }
+        else
+        {
+            // If not looking back, smoothly interpolate back to the original rotation
+            Quaternion originalRotation = Quaternion.Euler(xRotation, 0, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, Time.deltaTime * LookbackTransitionSpeed);
+        }
+    }*/
+    bool isTransitioning = false;
+
+    void HandleLookback()
+    {
+        Lookback = Input.GetKey(KeyCode.LeftAlt);
+
+        if (Lookback && !isTransitioning)
+        {
+            StartCoroutine(LookBackSmoothly());
+        }
+        else if (!Lookback && isTransitioning)
+        {
+            StartCoroutine(ReturnToForwardSmoothly());
+        }
+    }
+
+    IEnumerator LookBackSmoothly()
+    {
+        isTransitioning = true;
+
+        // Calculate the target rotation for looking back
+        Quaternion lookBackRotation = Quaternion.LookRotation(LookBackDirection.forward, transform.up);
+
+        // Smoothly interpolate between the current rotation and the target rotation
+        float elapsedTime = 0f;
+        Quaternion startRotation = transform.rotation;
+
+        while (elapsedTime < 1f)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, lookBackRotation, elapsedTime);
+            elapsedTime += Time.deltaTime * LookbackTransitionSpeed;
+            yield return null;
+        }
+
+        isTransitioning = false;
+    }
+
+    IEnumerator ReturnToForwardSmoothly()
+    {
+        isTransitioning = true;
+
+        // Smoothly interpolate back to the original rotation
+        float elapsedTime = 0f;
+        Quaternion startRotation = transform.rotation;
+        Quaternion originalRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        while (elapsedTime < 1f)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, originalRotation, elapsedTime);
+            elapsedTime += Time.deltaTime * LookbackTransitionSpeed;
+            yield return null;
+        }
+
+        isTransitioning = false;
     }
 
     #endregion
