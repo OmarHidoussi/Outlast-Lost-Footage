a013a38785a25c986b700fa158c77abb52c78a92
OmarHidoussi <gover8603@gmail.com>
2023-12-27 00:35:29 +0100

    Upload 9.5

---
+++ Outlast Lost Footage/Assets/Scripts/Music&Soundtracks/Chase.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Chase : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioSource PlayerSource;
+    public AudioClip Breathing;
+    public AudioClip Soundtrack;
+    public EnemySight Behavior;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Behavior.PlayerInSight || Behavior.LastSightPosition != Behavior.resetPosition)
+        {
+            source.volume = Mathf.Lerp(source.volume, 0.2f, 2f * Time.deltaTime);
+
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+        }
+        else
+        {
+            source.volume = Mathf.Lerp(source.volume, 0, 2 * Time.deltaTime);
+            source.Stop();
+        }
+    }
+}
