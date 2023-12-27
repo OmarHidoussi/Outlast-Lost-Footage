a0b6ee9f10f12d3901370bbefa0d347c0ced7b9d
OmarHidoussi <gover8603@gmail.com>
2023-12-27 17:19:36 +0100

    9.9
    Start Setting Up the Enemy

--- Outlast Lost Footage/Assets/Scripts/Enemy/EnemyAnimation.cs
+++ Outlast Lost Footage/Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAnimation : MonoBehaviour
 {
 
     #region Variables
+
+    public Animator Anim;
+    private NavMeshAgent nav;
 
     #endregion
 
@@ -14,13 +18,14 @@ public class EnemyAnimation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Anim.speed = nav.speed;
+        Anim.SetFloat("Speed", nav.speed);
     }
 
     #endregion
