c86ae09b9a30af457f1d321b6a60e8a6ec8da32e
OmarHidoussi <gover8603@gmail.com>
2024-02-04 16:11:47 +0100

    Upload 19.5

---
+++ Outlast Lost Footage/Assets/Scripts/Environment/BarredLight.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarredLight : MonoBehaviour
+{
+
+    #region Variables
+
+    public Light m_light;
+    public float MinTime = 0.5f;
+    public float Threshold = 0.5f;
+
+    public GameObject[] LightSource_OBJ;
+    public bool HasSource;
+
+    private float lastTime = 0;
+
+    #endregion
+
+    #region BuiltInMethods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Time.time - lastTime) > MinTime)
+        {
+            if(Random.value > Threshold)
+            {
+                m_light.enabled = false;
+                lastTime = Time.time;
+
+                if (HasSource)
+                {
+                    foreach(GameObject light in LightSource_OBJ)
+                    {
+                        light.SetActive(false);
+                    }
+                }
+            }
+            else
+            {
+                m_light.enabled = true;
+
+                if (HasSource)
+                {
+                    foreach (GameObject light in LightSource_OBJ)
+                    {
+                        light.SetActive(true);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    #endregion
+
+    #region CustomMethods
+
+    #endregion
+
+}
