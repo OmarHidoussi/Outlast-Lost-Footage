04d4f18f680f1aadf6850cb94d6ab94af4ffd259
OmarHidoussi <gover8603@gmail.com>
2024-06-01 12:53:09 +0200

    Upload 22.2

---
+++ Outlast Lost Footage/Assets/Scripts/Atomic Components/AnimationEvent.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour
+{
+
+    public Animator anim;
+    public string animationName;
+    public AudioSource source;
+    public AudioClip clip;
+
+    private bool HasPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HasPlayed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (!HasPlayed)
+            {
+                anim.SetBool(animationName, true);
+                if (source != null)
+                {
+                    source.clip = clip;
+                    source.PlayOneShot(clip);
+                    HasPlayed = true;
+                }
+            }
+        }
+    }
+}
